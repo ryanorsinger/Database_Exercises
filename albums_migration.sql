aa5a889c60646549c5eab81c739b3100e0ffa388
ryanorsinger <ryan.orsinger@gmail.com>
2015-03-30 11:11:35 -0500

    Add migration and seeder for albums table example

---
+++ albums_migration.sql
@@ -0,0 +1,13 @@
+USE codeup_test_db;
+
+DROP TABLE IF EXISTS albums;
+
+CREATE TABLE albums (
+    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    artist VARCHAR(100) NOT NULL,
+    name VARCHAR(100) NOT NULL,
+    sales_in_millions FLOAT(15,2) NOT NULL,
+    release_date YEAR(4) NOT NULL,
+    genre CHAR(255) NOT NULL,
+    PRIMARY KEY (id)
+);
